bff2a0e54590145be55353863eed71a67caa7583
Carl Simpson <carl.simpson@maginus.com>
2020-03-30 19:09:33 +0100

    MasteringITDesire - #BUG: Internal Link breaks

--- app/etc/config.php
+++ app/etc/config.php
@@ -351,6 +351,7 @@ return [
         'MDev_Helloworld' => 1,
         'Magento_PaypalReCaptcha' => 1,
         'MSP_TwoFactorAuth' => 1,
+        'Mastering_Itdesire' => 1,
         'Temando_Shipping' => 1,
         'Vertex_Tax' => 1,
         'Vertex_AddressValidation' => 1,
